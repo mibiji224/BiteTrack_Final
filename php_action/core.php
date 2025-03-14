1c30be0d4e9ae251cee946529ade3caccf48d5fd
Zai <108124820+mibiji224@users.noreply.github.com>
2025-03-14 12:40:14 +0800

    as

--- php_action/core.php
+++ php_action/core.php
@@ -7,7 +7,7 @@ require_once 'db_connect.php';
 // echo $_SESSION['userId'];
 
 if(!$_SESSION['userId']) {
-	header('Location: http://nutritracker.fwh.is/index.php');
+	header('Location: /index.php');
     exit();
 } 
 
